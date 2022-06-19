a24cae5d36316ec6723cac25b6e98359c3dd1f51
Ruediger Marwein <ruediger_marwein@hugoboss.com>
2022-06-19 09:18:06 +0200

    removed dependency to requests library.

--- sfcc-fetch-siteprefs/fetch-siteprefs.py
+++ sfcc-fetch-siteprefs/fetch-siteprefs.py
@@ -2,6 +2,7 @@
 python3.exe ./fetch-siteprefs.py development staging
 
 Systems from systems.py:acquirationConfig
+
 """
 
 """ OCAPI Permissions required:
@@ -14,8 +15,13 @@ Systems from systems.py:acquirationConfig
         }
 """
 import socket
-import requests
-from requests.auth import HTTPBasicAuth
+
+import urllib
+import urllib.parse
+import urllib.request
+import http.client
+from base64 import b64encode
+
 import json
 import csv
 import time
@@ -45,18 +51,30 @@ def getAccessToken(server):
 	accessTokenTimeout = ocapi_config[server]["accessTokenTimeout"] if "accessTokenTimeout" in ocapi_config[server] else None
 	if not accessToken or int(time.time()) > accessTokenTimeout:
 		pass_token = ocapi_config[server]["passToken"]
-		url_getToken = 'https://'+domain_ocapi+'/dw/oauth2/access_token?client_id='+id_ocapi+'&grant_type=urn:demandware:params:oauth:grant-type:client-id:dwsid:dwsecuretoken'
-		r = requests.post(url_getToken, 
-			auth = HTTPBasicAuth(user, password+":"+pass_token),
-			headers = {
-				'Content-Type': 'application/x-www-form-urlencoded',
-				'grant_type': 'urn:demandware:params:oauth:grant-type:client-id:dwsid:dwsecuretoken',
-			},
-			proxies=proxies
-		)
-		token = r.json()
-		if('access_token' not in token):
-			print("FATAL; Query for access token failed; Response: " + r.text + ";;\n")
+		user_and_pass = b64encode((user+":"+password+pass_token).encode('ascii')).decode('ascii')
+		getTokenQueryParts = {
+			'client_id':id_ocapi, 
+			'grant_type':'urn:demandware:params:oauth:grant-type:client-id:dwsid:dwsecuretoken'
+		}
+		getTokenQuery = urllib.parse.urlencode(getTokenQueryParts)
+		getTokenHeaders = {
+			'Authorization': 'Basic %s' % user_and_pass,
+			'Content-Type': 'application/x-www-form-urlencoded',
+		}
+		url_getToken = 'https://'+domain_ocapi+'/dw/oauth2/access_token?'+getTokenQuery
+		request = urllib.request.Request(url_getToken, data=None, headers=getTokenHeaders, method="POST")
+		if proxies:
+			request.set_proxy(proxies["https"], 'https')
+		token = None
+		response = None
+		try:
+			response = urllib.request.urlopen(request)
+			token = json.loads(response.read())
+		except urllib.error.HTTPError as e:
+			print(e.code)
+			print(e.read())  
+		if not token or 'access_token' not in token:
+			print("FATAL; Query for access token failed; Response: " + str(response.text) + ";;\n")
 		accessToken = token['access_token']
 		accessTokenTimeout = int(time.time())+300
 		ocapi_config[server]["accessToken"] = accessToken
@@ -67,15 +85,21 @@ def findPreferences(server, site, group, system):
 	global proxies
 	domain_ocapi = ocapi_config[server]["domain"]
 	url_preferenceSearch = 'https://'+domain_ocapi+'/s/-/dw/data/v20_3/sites/'+site+'/site_preferences/preference_groups/'+group+'/'+system+''
-	r = requests.get(url_preferenceSearch,
-		headers = {
-			'Authorization': 'Bearer '+getAccessToken(server),
-			'Content-Type': 'application/json'
-		},
-		proxies=proxies
-	)
-	r.close()
-	return r.text
+	headers = {
+		'Authorization': 'Bearer '+getAccessToken(server),
+		 'Content-Type': 'application/json'
+	}
+	request = urllib.request.Request(url_preferenceSearch, None, headers, method="GET")
+	if proxies:
+		request.set_proxy(proxies["https"], 'https')
+	jsonText = None
+	try:
+		response = urllib.request.urlopen(request)
+		jsonText = json.loads(response.read())
+	except urllib.error.HTTPError as e:
+		print(e.code)
+		print(e.read())
+	return json.dumps(jsonText)
 
 for server in fetchServers:
 	f = open("data-"+server+".jsonp", "w", encoding='utf-8')
